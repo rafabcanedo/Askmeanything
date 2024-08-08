f34b108134f8aa13f556b4615d23415e9ee50983
rafabcanedo <rafaitecanedo@gmail.com>
2024-08-08 08:50:34 -0300

    aula 1 - all front is already

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "sonner"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { CreateRoom } from "./pages/create-room"
 import { Room } from "./pages/room"
@@ -15,6 +16,9 @@ const router = createBrowserRouter([
 
 export function App() {
   return (
+   <>
    <RouterProvider router={router} />
+   <Toaster invert richColors />
+   </>
   )
 }
--- web/src/pages/room.tsx
+++ web/src/pages/room.tsx
@@ -1,5 +1,81 @@
+import { useParams } from 'react-router-dom'
+import amaLogo from '../assets/ama-logo.svg'
+import { toast } from 'sonner'
+import { ArrowRight, Share2 } from 'lucide-react'
+import { Message } from '../components/messages'
+
 export function Room() {
+ const { roomId } = useParams()
+
+ function handleShareRoom() {
+ // Pegando a url atual
+  const url = window.location.href.toString()
+
+  if (navigator.share !== undefined && navigator.canShare()) {
+   navigator.share({ url })
+  } else {
+   navigator.clipboard.writeText(url)
+
+   toast.success('The room URL was copied to your clipboard!')
+  }
+
+ }
  return (
-  <h1>Room</h1>
+  <div className="mx-auto max-w-[648px] flex flex-col gap-6 py-10 px-4">
+   <div className="flex items-center gap-3 px-3">
+    <img src={amaLogo} alt="AMA" className='h-5' />
+
+    <span className='text-sm text-zinc-500 truncate'>
+     Código da sala: <span className='text-zinc-300'>{roomId}</span>
+    </span>
+
+    <button 
+     type="submit"
+     onClick={handleShareRoom}
+     className='ml-auto bg-zinc-800 text-zinc-300 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm transition-colors hover:bg-zinc-700'
+    >
+     Compartilhar
+     <Share2 className='size-4' />
+    </button>
+   </div>
+
+   <div className='h-px w-full bg-zinc-900' />
+
+   <form
+    className='flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:ring-1'
+   >
+    <input
+     type="text"
+     name="theme"
+     placeholder="Qual a sua pergunta?"
+     autoComplete='off'
+     className='flex-1 text-sm bg-transparent mx-2 outline-none text-zinc-100 placeholder:text-zinc-500'
+    />
+
+    <button type="submit" className='bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm transition-colors hover:bg-orange-500'>
+     Criar pergunta
+     <ArrowRight className='size-4' />
+    </button>
+   </form>
+
+   <ol className='list-decimal list-outside px-3 space-y-8'>
+   <Message
+    text="Como funcionam as goroutines em GoLang e por que elas são importantes para a concorrência e paralelismo?" 
+    amountOfReactions={100}
+    answered
+   />
+
+   <Message 
+    text="Quais são as melhores práticas para organizar o código em um projeto GoLang, incluindo pacotes, módulos e a estrutura de diretórios?" 
+    amountOfReactions={50}
+   />
+
+   <Message 
+    text="O que é GoLang e quais são suas principais vantagens em comparação com outras linguagens de programação como Python, Java ou C++?" 
+    amountOfReactions={10}
+   />
+
+   </ol>
+  </div>
  )
 }
