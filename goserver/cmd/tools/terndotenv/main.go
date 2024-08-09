aae4bdb4d293fab49d8147693e0aa0ab5b1ad28a
rafabcanedo <rafaitecanedo@gmail.com>
2024-08-09 17:51:18 -0300

    aula 1 ready

--- goserver/cmd/tools/terndotenv/main.go
+++ goserver/cmd/tools/terndotenv/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"os/exec"
 
 	"github.com/joho/godotenv"
@@ -13,12 +11,6 @@ func main() {
 		panic(err)
 	}
 
-	dir, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Diretorio de trabalho atual:", dir)
-
 	cmd := exec.Command(
 		"tern",
 		"migrate",
@@ -27,19 +19,7 @@ func main() {
 		"--config",
 		"./internal/store/pgstore/migrations/tern.conf",
 	)
-
-	fmt.Println("Executando comando:", cmd.String())
-
-	//if err := cmd.Run(); err != nil {
-	//	panic(err)
-	//}
-
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		fmt.Printf("Error ao executat comando: %s\n", err)
-		fmt.Printf("Saida do comando: %s\n", output)
+	if err := cmd.Run(); err != nil {
 		panic(err)
 	}
-
-	fmt.Println("Saida do comando:", string(output))
 }
